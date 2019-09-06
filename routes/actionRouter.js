c6baa6f9891f0e8082a4996d1080f6562a245252
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-06 09:26:44 -0700

    added get request for /actions

--- routes/actionRouter.js
+++ routes/actionRouter.js
@@ -5,7 +5,13 @@ const router = express.Router();
 const Actions = require('../data/helpers/actionModel.js');
 
 router.get('/', (req, res) => {
-    
+    Actions.get()
+    .then(actions => {
+        res.status(200).json(actions);
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'Actions data could not be retrieved from the server.' });
+    })
 })
 
 
