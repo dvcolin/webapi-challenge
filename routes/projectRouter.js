91f4c84828fdf58ea246aa497f5b293f7018ba0a
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-06 08:52:51 -0700

    added get request to /projects and /projects/id. added middleware to validate the project id

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -14,5 +14,36 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', validateProjectId, (req, res) => {
+    const project = req.project;
+
+    Projects.get(project.id)
+    .then(project => {
+        res.status(200).json(project);
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'Project data could not be retrieved from the server' })
+    })
+})
+
+
+// middleware
+
+function validateProjectId(req, res, next) {
+    const { id } = req.params;
+
+    Projects.get(id)
+    .then(project => {
+        if (project) {
+            req.project = project;
+            next();
+        } else {
+            res.status(404).json({ message: 'A project with that ID could not be found.' });        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'Project data could not be accessed.' });
+    })
+}
+
 
 module.exports = router;
