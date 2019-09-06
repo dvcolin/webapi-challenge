7d5c8d6787f09041ef2d7a2ed611af72fac73a03
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-09-06 09:34:09 -0700

    added get request to /actions/id

--- routes/actionRouter.js
+++ routes/actionRouter.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 const Actions = require('../data/helpers/actionModel.js');
 
+// GET /actions
+
 router.get('/', (req, res) => {
     Actions.get()
     .then(actions => {
@@ -13,6 +15,43 @@ router.get('/', (req, res) => {
         res.status(500).json({ error: 'Actions data could not be retrieved from the server.' });
     })
 })
+
+
+// GET /actions/id
+
+router.get('/:id', validateActionId, (req, res) => {
+    const action = req.action;
+
+    Actions.get(action.id)
+    .then(action => {
+        res.status(200).json(action);
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'Action data could not be retrieved from the server.' });
+    })
+})
+
+
+// middleware
+
+function validateActionId (req, res, next) {
+    const { id } = req.params;
+
+    Actions.get(id)
+    .then(action => {
+        if (action) {
+            req.action = action;
+            next();
+        } else {
+            res.status(404).json({ message: 'An action with that ID could not be found.' });
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'Action data could not be retrieved from the server.' });
+    })
+}
+
+
 
 
 module.exports = router;
